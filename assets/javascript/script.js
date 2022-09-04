dd1a93979d18a7fc02ad37717ff86ded25383e50
Carl-Tallman-95 <carl.kristiansson1@gmail.com>
2022-09-04 15:49:05 +0000

    Add project variables in javascript

---
+++ assets/javascript/script.js
@@ -0,0 +1,17 @@
+const play = document.getElementById("play");
+
+const quiz = document.getElementById("quiz");
+
+const question = document.getElementById("question");
+
+const answerA = document.getElementById("answerA");
+const answerB = document.getElementById("answerB");
+const answerC = document.getElementById("answerC");
+const answerD = document.getElementById("answerD");
+
+const submitButton = document.getElementById("submit-button");
+
+const progress = document.getElementById("progress");
+
+const scoreContainer = document.getElementById("scoreContainer");
+
